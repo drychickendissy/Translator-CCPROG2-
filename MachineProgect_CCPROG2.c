642624eabfcb52062d6dbf28fba2801a520f2393
DryChicken <Notjoe0624@gmail.com>
2025-02-17 14:56:48 +0800

    Add files via upload

---
+++ MachineProgect_CCPROG2.c
@@ -0,0 +1,13 @@
+/*********************************************************************************************************
+This is to certify that this project is our own work, based on our personal efforts in studying and applying the concepts
+learned. We have constructed the functions and their respective algorithms and corresponding code by ourselves. The
+program was run, tested, and debugged by our own efforts. I further certify that I have not copied in part or whole or
+otherwise plagiarized the work of other students and/or persons.
+Edmund Kendrick D. Bilanes, DLSU ID# 12414980
+<your full name>, DLSU ID# <number>
+*********************************************************************************************************/
+
+int
+main(){
+	
+}
